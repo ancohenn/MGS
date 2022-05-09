0c8931fb21788d36cb4820b8c60e56be3626199d
ancohenn <105071692+ancohenn@users.noreply.github.com>
2022-05-09 12:37:06 +0100

    Added Round 4 folder

---
+++ ROUND 4/SFGPS_Thinger_WORKING/arduino_secrets.h
@@ -0,0 +1,9 @@
+#define USERNAME "anc"
+#define DEVICE_ID "mkr1400gsm"
+#define DEVICE_CREDENTIAL "u_yPl%u?1TzyFT6r"
+
+#define PIN_NUMBER ""
+
+#define GPRS_APN "iot.1nce.net"
+#define GPRS_LOGIN ""
+#define GPRS_PASSWORD ""
